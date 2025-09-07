b54b7cb56f25a430b397676ac4745fd0f726baa4
Brad Traversy <me@bradTraversy.com>
2025-09-07 11:24:09 +0530

    Second commit

--- api/editor.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    // Example: handle editor logic and return success
-    res.status(200).json({ message: "Editor API working!" });
-  } else {
-    res.status(405).json({ error: "Method not allowed" });
-  }
-}
